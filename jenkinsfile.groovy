7e1fcb9bebacb97ea50be27e76d30cd7c9d0db98
Aishu937 <95344337+Aishu937@users.noreply.github.com>
2023-04-26 12:04:14 +0530

    Create jenkinsfile.groovy

---
+++ jenkinsfile.groovy
@@ -0,0 +1,31 @@
+pipeline {
+    agent {label 'masternodes'}
+    
+    stages {
+      stage('parallel Task A & C') {
+          parallel {
+              stage('Print Job Name & Node Name') {
+                  steps {
+                      sh 'echo "The ${JOB_NAME} run on ${NODE_NAME}"'
+                       echo "the global email is ${var_email}"
+                      
+                    }
+                  
+               }
+              stage ("create file"){
+                   steps{
+                      sh 'ls -la > command.output'
+                    }
+                }    
+            }
+        }    
+        stage ("copy file to jenkins"){
+            steps{
+                echo "the global email is ${var_email}"
+              sh "ssh jenkins@192.168.1.27 mkdir -p aishwarya"
+                 sh "/usr/bin/scp /home/node1/jenkins-node/workspace/copycommandoutput/command.output jenkins@192.168.1.27:/home/jenkins/aishwarya"
+
+            }
+        }
+    }
+}
